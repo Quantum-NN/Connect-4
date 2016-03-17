4e8247d7850ea676691260d387ef1f75d25fe0e2
Quantum-NN <niluckshan8e@gmail.com>
2016-03-17 15:44:38 -0400

    Added files via upload
    JFrame Code

---
+++ Frame.java
@@ -0,0 +1,18 @@
+package Connect.Four;
+
+import javax.swing.*;
+
+
+public class Frame {
+
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame ("Quantum Connect 4");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(new Panel());
+		frame.pack();
+		frame.setResizable(true);
+		frame.setVisible(true);
+	}
+}
