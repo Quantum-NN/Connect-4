98e3fdda4bf3ae3e4fd55ce89294eb5292058257
Quantum-NN <niluckshan8e@gmail.com>
2016-03-21 13:25:47 -0400

    hh

--- Connect 4/src/connectFour/Frame.java
+++ Connect 4/src/connectFour/Frame.java
@@ -6,7 +6,7 @@ public class Frame {
 
 	public static void main(String[] args) {
 			
-			JFrame frame = new JFrame ("Quantum Connect 4");
+			JFrame frame = new JFrame ("Quantum Connect 44");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			//frame.getContentPane().add(new Panel());
 			frame.pack();
