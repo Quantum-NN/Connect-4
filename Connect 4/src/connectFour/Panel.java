0c757fbe5508c185127a71d16d8cb06ab19323e8
Quantum-NN <niluckshan8e@gmail.com>
2016-03-29 13:12:54 -0400

    Updated Code for AAUUUMMM

--- Connect 4/src/connectFour/Panel.java
+++ Connect 4/src/connectFour/Panel.java
@@ -17,8 +17,8 @@ public class Panel extends JPanel {
 	private JButton leaderboardsBttn;
 	
 	public Panel(){
+		
 	String name = "";	
-	JButton[][] buttons = new JButton [6][7];
 	setLayout (new GridBagLayout());
 	GridBagConstraints gbc = new GridBagConstraints();
 	gbc.insets = new Insets (5,5,5,5);
@@ -28,39 +28,47 @@ public class Panel extends JPanel {
 	
 	ImageIcon instructions = new ImageIcon (System.getProperty("user.dir") + "\\Instructions Button.png");
 	JButton instructionsButton = new JButton (instructions);
-	add(instructionsButton, gbc);
 	instructionsButton.addActionListener(new ButtonListener());
 	
 	
 	ImageIcon singlePlayer = new ImageIcon (System.getProperty("user.dir") + "\\Single Player.png");
 	JButton singlePlayerBttn = new JButton (singlePlayer);
-	gbc.gridx = 0;
-	gbc.gridy = 1;
-	add(singlePlayerBttn, gbc);
+	singlePlayerBttn.addActionListener(new ButtonListener());
 	
 	ImageIcon twoPlayer = new ImageIcon (System.getProperty("user.dir") + "\\Two Player.png");
 	JButton twoPlayerBttn = new JButton (twoPlayer);
-	gbc.gridx = 0;
-	gbc.gridy = 2;
-	add(twoPlayerBttn, gbc);
+	twoPlayerBttn.addActionListener(new ButtonListener());
 	
 	ImageIcon leaderboards = new ImageIcon (System.getProperty("user.dir") + "\\Leaderboards.png");
 	JButton leaderboardsBttn = new JButton (leaderboards);
-	gbc.gridx = 0;
-	gbc.gridy = 3;
-	add(leaderboardsBttn, gbc);	
+	leaderboardsBttn.addActionListener(new ButtonListener());
 	
 	ImageIcon intro = new ImageIcon (System.getProperty("user.dir") + "\\titlee.png");
 	JLabel introLabel = new JLabel(intro);
+	
+	
 	gbc.gridx = 0;
 	gbc.gridy = 0;
-	add(introLabel, gbc);
-	
+	add(introLabel, gbc);	
+	gbc.gridx = 0;
+	gbc.gridy = 1;
+	add(instructionsButton, gbc);
+	gbc.gridx = 0;
+	gbc.gridy = 2;
+	add(singlePlayerBttn, gbc);
+	gbc.gridx = 0;
+	gbc.gridy = 3;
+	add(twoPlayerBttn, gbc);
+	gbc.gridx = 0;
+	gbc.gridy = 4;
+	add(leaderboardsBttn, gbc);	
+
 	}
 	
 	private class ButtonListener implements ActionListener {
 		
 		public void actionPerformed(ActionEvent event) {
+		
 		
 		}
 	}
